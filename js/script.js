5db90ad3619f1e4f1420a827a18e74daa5f637ad
Thasyo Peres <thasyotp2002@gmail.com>
2022-12-20 01:32:26 -0300

    add function addTask

---
+++ js/script.js
@@ -0,0 +1,51 @@
+//função para adicionar tarefas.
+const addTask = () => {
+
+    //adicionando tarefa ao elemento.
+    const taskTitle = document.querySelector(".task-title").value;
+
+    if(taskTitle){
+
+        //Colocando conteudo no elemento.
+        const resultContent = document.querySelector("#result-content");
+        resultContent.textContent = `${taskTitle}`; 
+
+        //clonando template.
+        const addedTask = document.querySelector("#added-task");
+        const newTask = addedTask.cloneNode(true); //clona o html selecionado e implementa em uma nova variável.
+
+        //adicionando conteudo da nova tarefa.
+        newTask.querySelector("#result-content").textContent = `${taskTitle}`;
+
+        //removendo classe desnecessária.
+        newTask.removeAttribute("style");
+
+        //Adicionando nova tarefa.
+        const tasksListContainer = document.querySelector("#tasks-list-container");
+        tasksListContainer.appendChild(newTask);
+
+        //limpando input após adicionar tarefa.
+        document.querySelector(".task-title").value = ""; 
+
+    }else{
+        alert("Preencha o campo vazio.");
+    };
+
+
+
+}
+//função para remover tarefas.
+
+//função para finalizar e "desfinalizar" tarefas.
+
+
+//selecionando elementos.
+const addBtn = document.querySelector("#add-btn");
+
+//adicionando tarefas.
+addBtn.addEventListener("click", (e) => {
+
+    e.preventDefault();
+    addTask();
+
+});
