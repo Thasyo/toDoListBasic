38a5ca72254a374eda8e9d48e3b9d5d9d8a857c5
Thasyo Peres <thasyotp2002@gmail.com>
2022-12-22 00:25:58 -0300

    add function doneTask

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ const addTask = () => {
     if(taskTitle){
 
         //Colocando conteudo no elemento.
-        const resultContent = document.querySelector("#result-content");
+        const resultContent = document.querySelector(".result-content");
         resultContent.textContent = `${taskTitle}`; 
 
         //clonando template.
@@ -15,7 +15,7 @@ const addTask = () => {
         const newTask = addedTask.cloneNode(true); //clona o html selecionado e implementa em uma nova variável.
 
         //adicionando conteudo da nova tarefa.
-        newTask.querySelector("#result-content").textContent = `${taskTitle}`;
+        newTask.querySelector(".result-content").textContent = `${taskTitle}`;
 
         //removendo classe desnecessária.
         newTask.removeAttribute("style");
@@ -28,6 +28,12 @@ const addTask = () => {
         const removeBtn = newTask.querySelector(".remove-btn");
         removeBtn.addEventListener("click", () => {
             removeTask(removeBtn);
+        });
+
+        //Adicionando evento de finalização de tarefas.
+        const doneBtn = newTask.querySelector(".done-btn");
+        doneBtn.addEventListener("click", () => {
+            doneTask(doneBtn);
         });
 
         //limpando input após adicionar tarefa.
@@ -45,8 +51,13 @@ const removeTask = (removeBtn) => {
     removeNewTask.remove();
 };
 
-//função para finalizar e "desfinalizar" tarefas.
-
+//função para finalizar tarefas.
+const doneTask = (doneBtn) =>{
+    const fatherBtn = doneBtn.parentNode;
+    const completeNewTask = fatherBtn.parentNode;
+    const spanContent = completeNewTask.querySelector(".div-task-title-task .result-content");
+    spanContent.style.cssText = "text-decoration: line-through; color: #000000d2;";
+};
 
 //selecionando elementos.
 const addBtn = document.querySelector("#add-btn");
